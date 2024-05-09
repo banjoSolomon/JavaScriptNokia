cb71dc44e37145117f19a1a2ca2d663b22962e3c
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-09 14:08:42 +0100

    javaScript

---
+++ async.js
@@ -0,0 +1,14 @@
+setTimeout(()=>{
+    console.log("Hello World");
+}, 1000)
+
+setTimeout(()=>{
+    console.log("Hello World1");
+}, 2000)
+
+
+setTimeout(()=>{
+    console.log("Hello World2");
+}, 500)
+
+//async / await
