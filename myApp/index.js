750ba536f421848322f1db3b5fa9d0d4bce9a069
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-09 15:15:05 +0100

    javaScript

--- myApp/index.js
+++ myApp/index.js
@@ -1,2 +1,10 @@
 const form = document.forms["form"]
-console.log(form)
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    const firstName = document.getElementById("firstName").value;
+    console.log(firstName)
+    const lastName = document.querySelector(".lastName").value;
+    //const last = document.getElementsByClassName("lastName")[0].value
+    console.log(lastName)
+})
